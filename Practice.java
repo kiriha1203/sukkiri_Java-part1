52fe2df873265d56df1fdb2eda1b635979637c90
kiriha1203 <mizumoto.takefumi@gmail.com>
2020-09-15 21:45:19 +0900

    a

---
+++ Practice.java
@@ -0,0 +1,22 @@
+public class Practice {
+  public static void main(String[] args) {
+    practice2();
+    practice3();
+  }
+
+
+  private static void practice2() {
+    int a = 3; int b = 5;
+    int c = a * b;
+    System.out.println("縦幅3横幅5の長方形の面積は、" + c);
+}
+
+
+  private static void practice3() {
+    boolean result = true;
+    char first = '駆';
+    double pi = 3.14;
+    long number = 31459265853979L;
+    String msg = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+  }
+}
---
+++ Test2.java
@@ -0,0 +1,9 @@
+public class Test2 {
+  public static void main (String[] args) {
+    int age = 20;
+    System.out.println("私の年齢は" + age);
+    age = 31;
+    System.out.println("・・・いや本当は" + age);
+    System.out.println("???");
+  }
+}
